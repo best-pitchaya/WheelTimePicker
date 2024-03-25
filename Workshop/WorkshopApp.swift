9777793e9763169778dfe51a8dd1fbd115949340
Best Chantanapongvanij <best.pc2610@gmail.com>
2024-03-25 19:35:19 -0400

    Initial Commit

---
+++ Workshop/WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopApp.swift
+//  Workshop
+//
+//  Created by Best Chantanapongvanij on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
